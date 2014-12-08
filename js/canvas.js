64bc34bf7b7f9c0bd47038039c809e989a7e6a9e
Dana Engel <engel.dana.mail@gmail.com>
2014-12-08 22:32:32 +0100

    edit spelling error and remove black/white-functionality because it won't work

--- js/canvas.js
+++ js/canvas.js
@@ -1,5 +1,5 @@
 function drawImage(imageObj) {
-  var canvas = document.getElementById('itecCanvas');
+  var canvas = document.getElementById('itec-canvas');
   var context = canvas.getContext('2d');
   
 
@@ -15,17 +15,6 @@ function drawImage(imageObj) {
   
   var imageData = context.getImageData(x, y, w, h);
   var data = imageData.data;
-  
-  // bild schwarzweiss machen (FIXME fuer Schluss-Abgabe)
-  /* for(var i = 0; i < data.length; i += 4) {
-    var brightness = 0.34 * data[i] + 0.5 * data[i + 1] + 0.16 * data[i + 2];
-    data[i] = brightness;
-    data[i + 1] = brightness;
-    data[i + 2] = brightness;
-  }
-  
-  context.putImageData(imageData, x-64, y-64);
-  */
 }
    
 var imageObj = new Image();
